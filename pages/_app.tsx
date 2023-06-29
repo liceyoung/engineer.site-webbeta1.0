0f9612f7a984e5ed79be3ddfeea92e0a005133c5
Thao (Alice) Thach Duong <84640455+liceyoung@users.noreply.github.com>
2023-06-29 12:51:24 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <React.Fragment>
       <Head>
-        <title>abc</title>
+        <title> Engineering Program </title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
